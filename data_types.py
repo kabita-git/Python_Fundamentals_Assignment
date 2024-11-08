e170ed2ad35de5052e852ee44c2f20e7f10f2df2
kabita-git <kabita_btechedit2023@kusoed.edu.np>
2024-11-08 22:33:36 +0545

    Completed Task 1 - Data Types.

---
+++ data_types.py
@@ -0,0 +1,26 @@
+#TASK-1
+
+#Creating  Variables 
+a = 10  
+b = 5 
+c = "Good Morning"  
+d = True  
+
+# Operations
+#Arithmetic Operation
+print(a + b)
+print(a*b)
+print(a-b)
+print(a/b)
+
+#String Concatenation
+print(c + " Everyone") 
+
+#Logical Oerations
+print(d or False) 
+print(d and False)   
+print(not d)
+
+# Creating Dictionary
+data = {"int": [a], "float": [b], "str": [c], "bool": [d]}
+print(data)
